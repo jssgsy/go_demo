5d82c820873f811a3277a16429aa1cca5fb13cbf
univ <fcsnwu@163.com>
2018-08-30 14:57:54 +0800

    [if,for,switch等]可以有初始化语句

--- src/test/how_to_test.go
+++ src/test/how_to_test.go
@@ -1,7 +1,10 @@
 package test
 
 //1. 必须引入testing包
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 /*
  * 2. 测试方法必须以Test开头，后面部分必须以大写字母开头
@@ -21,4 +24,16 @@ func TestDemo2(t *testing.T) {
 	} else {
 		t.Error("测试失败了")
 	}
+}
+
+/**
+测试条件表示式
+1. Go 完全省略了 if、switch 和 for 结构中条件语句两侧的括号
+*/
+func TestCondition(t *testing.T) {
+	if i := 1; i >= 0 {
+		fmt.Println("go中的条件表示式可以有初始化条件")
+	}
+	// 在if外读取不到初始化条件中的变量i
+	//fmt.Println(i)
 }
